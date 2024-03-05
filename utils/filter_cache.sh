8c7516202cc054ac6cf0a679317e40c437ed91d9
YangXiran <yangxr@stu.pku.edu.cn>
2024-03-05 19:10:13 +0800

    modify filter_cache.sh: add $out_dir

--- utils/filter_cache.sh
+++ utils/filter_cache.sh
@@ -3,12 +3,19 @@
 # 定义输入文件夹路径
 input_dir="/home/yangxr/projects/learned_dram/hot_dist/hot_trace/trace/mcf.in"
 
+# 定义输出文件夹
+out_dir="./data/mcf"
+if [ ! -d $out_dir ];then
+		mkdir -p $out_dir
+fi
+
 # 定义程序路径（如果filter_cache在PATH中，直接使用filter_cache即可）
 filter_cache="./filter_cache"
 
 prefix="600s"
-paral_degree=20
+paral_degree=22
 echo "path: $input_dir"
+echo "out_dir: $out_dir" 
 echo "prefix: $prefix"
 echo "paral_degree: $paral_degree"
 
@@ -17,7 +24,7 @@ echo "paral_degree: $paral_degree"
 # -j16 指定同时运行16个作业
 # 使用{}占位符传递文件名给filter_cache，然后使用echo打印每个文件处理完成的消息
 # 使用\;来转义分号，以便将其包含在命令字符串中
-find "$input_dir" -name "${prefix}*.raw.trace" | parallel -j ${paral_degree} "${filter_cache} {} ; echo {} 已处理完"
+find "$input_dir" -name "${prefix}*.raw.trace" | parallel -j ${paral_degree} "${filter_cache} -o ${out_dir} {} ; echo {} 已处理完"
 
 echo "所有文件处理完成！"
 
