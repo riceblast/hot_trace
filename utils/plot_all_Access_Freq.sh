d2b7b76e5474d0258b5411c7179606768f11ad37
YangXiran <yangxr@stu.pku.edu.cn>
2024-06-17 22:33:46 +0800

    Add parallel support for 'plot_all_xxx' script

--- utils/plot_all_Access_Freq.sh
+++ utils/plot_all_Access_Freq.sh
@@ -1,15 +1,43 @@
 #benchname_list=(BFS cactuBSSN deepsjeng fotonik3d GUPS mcf PR XZ)
+#benchname_list=(liblinear_roi SSSP500 BFS500 redis)
+#benchname_list=(SSSP500)
+#benchname_list=(redis liblinear_roi XSBench_64G_roi BC_roi_4thr XSBench_64G_roi_4thr)
 benchname_list=(redis)
-type_list=('v' 'p')
-period_list=(1 5 10 15 30)
+type_list=('v')
+period_list=(15 30 60 120)
+#cacheblock_list=('256' '2M')
+cacheblock_list=('2M')
+threads_num=4
 
-for bench in "${benchname_list[@]}"
+echo "
+for benchname in ${benchname_list[@]}
 do
-		for addr_type in "${type_list[@]}"
-		do
-				for period in "${period_list[@]}"
-				do
-						python3 ./plot_global_cdf.py --type ${addr_type} --period ${period} ${bench}
-				done
-		done
+	parallel -j${threads_num} \
+		python3 /home/yangxr/projects/learned_dram/hot_dist/hot_trace/utils/plot_global_cdf.py \
+		--type ::: ${type_list[@]} \
+		::: --period ::: ${period_list[@]} \
+		::: --cacheblock ::: ${cacheblock_list[@]} \
+		::: ${benchname} 2>&1 >> log/all_access_freq.log
+
+	if [ $? -ne 0 ]
+	then
+		echo "${benchname} Error! exit"
+		exit 1
+	fi
+done" > log/all_access_freq.log
+
+for benchname in ${benchname_list[@]}
+do
+	parallel -j${threads_num} \
+		python3 /home/yangxr/projects/learned_dram/hot_dist/hot_trace/utils/plot_global_cdf.py \
+		--type ::: ${type_list[@]} \
+		::: --period ::: ${period_list[@]} \
+		::: --cacheblock ::: ${cacheblock_list[@]} \
+		::: ${benchname} 2>&1 >> log/all_access_freq.log
+
+	if [ $? -ne 0 ]
+	then
+		echo "${benchname} Error! exit"
+		exit 1
+	fi
 done
--- utils/plot_all_CDF.sh
+++ utils/plot_all_CDF.sh
@@ -1,17 +1,47 @@
 #benchname_list=(BFS cactuBSSN deepsjeng fotonik3d GUPS mcf PR XZ)
 #type_list=('v' 'p')
 #period_list=(1 5 10)
-benchname_list=(BC PR redis)
+#benchname_list=(liblinear_roi SSSP500 BFS500 redis)
+#benchname_list=(SSSP500)
+#benchname_list=(liblinear_roi XSBench_64G_roi BC_roi_4thr redis XSBench_64G_roi_4thr)
+benchname_list=(redis)
 type_list=('v')
-period_list=(1 5 15 30)
+period_list=(15 30 60 120)
+#cacheblock_list=('256' '2M')
+cacheblock_list=('2M')
+threads_num=4
 
-for bench in "${benchname_list[@]}"
+# for bench in "${benchname_list[@]}"
+# do
+# 		for addr_type in "${type_list[@]}"
+# 		do
+# 				for period in "${period_list[@]}"
+# 				do
+# 						python3 ./plot_hot_cdf.py --type ${addr_type} --period ${period} ${bench}
+# 				done
+# 		done
+# done
+
+for benchname in ${benchname_list[@]}
 do
-		for addr_type in "${type_list[@]}"
-		do
-				for period in "${period_list[@]}"
-				do
-						python3 ./plot_hot_cdf.py --type ${addr_type} --period ${period} ${bench}
-				done
-		done
+	echo "
+	parallel -j${threads_num} \
+		python3 /home/yangxr/projects/learned_dram/hot_dist/hot_trace/utils/plot_hot_cdf.py \
+		--type ::: ${type_list[@]} \
+		::: --period ::: ${period_list[@]} \
+		::: --cacheblock ::: ${cacheblock_list[@]} \
+		::: ${benchname} 2>&1 >> log/all_zipfan_hot_dist.log" > log/all_CDF.log
+
+	parallel -j${threads_num} \
+		python3 /home/yangxr/projects/learned_dram/hot_dist/hot_trace/utils/plot_hot_cdf.py \
+		--type ::: ${type_list[@]} \
+		::: --period ::: ${period_list[@]} \
+		::: --cacheblock ::: ${cacheblock_list[@]} \
+		::: ${benchname} 2>&1 >> log/all_CDF.log
+
+	if [ $? -ne 0 ]
+	then
+		echo "${benchname} Error! exit"
+		exit 1
+	fi
 done
